1f7978c4e90b82e97b92f4943c4068d0a99d08c3
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-06-06 13:24:36 -0500

    Final T2 avance t3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -19,7 +19,7 @@
 
     .row.justify-content-center.align-items-center.mb-5(data-aos="fade")
       .col-lg-2.col-md-3.col-sm-12
-        img(src='@/assets/curso/t2/img2.png', alt='')
+        img.img-t.img-a(src='@/assets/curso/t2/img2.png', alt='')
       .col-lg-7.col-md-9.col-sm-12
         .row.bgazulclaro.borde-redondo
           p.px-5.py-4.mb-0 En el área del patronaje, el sistema de medidas hace referencia al conjunto de técnicas y procesos utilizados para obtener, interpretar y aplicar las dimensiones corporales en la elaboración de patrones para prendas de vestir. La precisión en la toma de estas medidas es esencial, ya que de ello depende el ajuste, la horma y la correcta adaptación de la prenda al cuerpo, logrando así un equilibrio entre proporcionalidad, comodidad y estética.
@@ -68,6 +68,17 @@
           .col-lg-11.col-md-11.col-sm-12
             img.mb-4(src='@/assets/curso/t2/fig11.png', alt='La figura representa una escala graduada del sistema inglés que indica fracciones de pulgada desde 0 hasta 10 pulgadas. Cada pulgada está subdividida en dieciséis partes iguales, lo que permite medir con precisión múltiplos de 1/16 de pulgada. También se destacan las marcas intermedias correspondientes a 1/2 pulgada.')
 
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade")
+      .col-lg-5.col-md-7.col-sm-12
+        .row.bgverdeclaro.align-items-center.justify-content-center.px-5
+          .titulo-sexto.color-acento-contenido.mt-4
+            h5 Figura 12.
+            | Sistema fraccionado
+          .col-auto
+            img.mb-4.mt-2(src='@/assets/curso/t2/fig12.png', alt='Ilustración lineal en 2D de una circunferencia dividida en tres partes para explicar el sistema métrico por numerador y denominador.')
+      .col-lg-7.col-md-9.col-sm-12
+        img.p-1(src='@/assets/curso/t2/img7.png', alt='')
+
 
     .row.justify-content-center.align-items-center.bgprimario.mb-5(data-aos="fade-left")
       .col-lg-12.col-md-12.col-sm-12.px-5.mt-4
@@ -77,6 +88,136 @@
         .row.align-items-center.justify-content-center
           .col-lg-11.col-md-11.col-sm-12
             img.mb-4(src='@/assets/curso/t2/fig13.png', alt='La figura presenta una escala en centímetros, correspondiente al sistema métrico. La regla está graduada de 0 a 25 centímetros, con subdivisiones en milímetros, lo que permite mediciones precisas en décimas de centímetro. Cada número entero representa un centímetro completo.')
+
+    .row.bgverde.mx-5.borde-redondo.movercajonverde(data-aos="fade-right")
+      h3.text-white.text-center.px-5.py-4 Los dos tipos de sistemas son utilizados en diferentes áreas y cadenas productivas teniendo en cuenta que algunos estándares están trabajados en medidas diferentes a las que usa oficialmente el país.
+    .row.justify-content-center.align-items-center.bgrosado.movercajonrosado.px-4
+      p.mt-5
+      p.mt-4
+      .col-lg-5.col-md-12.col-sm-12.mt-2
+        .titulo-sexto.color-acento-contenido
+          h5 Tabla 2. 
+          |  Análisis de unidades
+        .tabla-a.color-secundario.estilo1.mb-4
+          table
+            thead
+              tr
+                th Nombre
+                th Abreviatura
+                th Equivalencia
+            tbody
+              tr
+                td Kilómetro
+                td km
+                td 1000 m
+              tr
+                td Hectómetro
+                td hm
+                td 100 m
+              tr
+                td Decámetro
+                td dam
+                td 10 m
+              tr
+                td Metro
+                td m
+                td 1 m
+              tr
+                td Decímetro
+                td dm
+                td 0,1 m
+              tr
+                td Centímetro
+                td cm
+                td 0,01 m
+              tr
+                td Milímetro
+                td mm
+                td 0,001 m
+
+      .col-lg-7.col-md-9.col-sm-12
+        img.mb-3(src='@/assets/curso/t2/img8.png', alt='')
+        .tarjeta.color-primario.p-3(data-aos="fade").mb-4
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-2
+              img(src="@/assets/curso/t2/pdf.png")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4.mb-1 Clasificación de las medidas
+                  p.mb-0 Lo invitamos a consultar el PDF - Anexo 1. Clasificación de las medidas, donde se presentan de forma detallada la clasificación general, los sistemas de medida, las medidas anatómicas y sus respectivas deducciones.
+                .col-sm-auto
+                  a.boton.color-acento-botones.texto-blanco(:href="obtenerLink('/downloads/1_Anexo.pdf')" target="_blank")
+                    span Descargar
+                    i.fas.fa-file-download
+
+    Separador.mt-0  
+    #t_2_2.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+      h2 2.2  Las medidas
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade")
+      .col-lg-3.col-md-5.col-12.p-0
+        img(src='@/assets/curso/t2/img9.png', alt='')
+      .col-lg-9.col-md-12.col-sm-12
+        .row.bgverdeclaro
+          p.px-5.py-4.mt-4.mb-2 Son unidades numéricas que representan el tamaño de un objeto, figura o parte específica; en el patronaje de prendas de vestir, estos datos se obtienen utilizando una cinta métrica de forma lineal para medir diferentes partes del cuerpo, considerando largo, ancho y contorno. En Colombia, es común emplear tanto centímetros como pulgadas para este proceso.
+
+    .subt.mb-5(data-aos="fade-right")
+      .row.align-items-center
+        .col-md-auto.col-3
+          img(src='@/assets/curso/t1/ico.png', alt='')
+        .col-md-auto.col-9.ps-0
+          h3.mb-0.text-black Medidas corporales
+
+    p.mb-4 Se obtienen a partir de los puntos de anclaje del cuerpo humano, considerando criterios antropométricos y anatómicos; estas medidas deben contemplar los volúmenes y protuberancias del cuerpo y se clasifican en tres grupos principales.
+
+    .row.mb-5(data-aos="fade-left")
+      .col-sm-6.col-md-12.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.bginactiva.p-4
+          .row.justify-content-center.mb-4
+            .col-auto
+              img(src='@/assets/curso/t2/img10.png', alt='Texto que describa la imagen')
+          .row.align-items-center.justify-content-center
+            .col-auto.bgverde
+              h3.px-5.text-white.py-0.mb-0 Largos
+          p.px-3.mb-4.mt-3.text-center Las medidas se toman de forma vertical y estas hacen referencia a los largos y talles del cuerpo.
+
+      .col-sm-6.col-md-12.col-lg-4.mb-4.mb-lg-0
+        .tarjeta--boton.bginactiva.p-4
+          .row.justify-content-center.mb-4
+            .col-auto
+              img(src='@/assets/curso/t2/img11.png', alt='Texto que describa la imagen')
+          .row.align-items-center.justify-content-center
+            .col-auto.bgverde
+              h3.px-5.text-white.py-0.mb-0 Anchos
+          p.px-3.mb-4.mt-3.text-center La medida se toma de manera horizontal y corresponden a los anchos del cuerpo, como el ancho de pecho.
+
+      .col-sm-6.col-md-12.col-lg-4.mb-4.mb-sm-0
+        .tarjeta--boton.bginactiva.p-4
+          .row.justify-content-center.mb-4
+            .col-auto
+              img(src='@/assets/curso/t2/img12.png', alt='Texto que describa la imagen')
+          .row.align-items-center.justify-content-center
+            .col-auto.bgverde
+              h3.px-5.text-white.py-0.mb-0 Contornos
+          p.px-3.mb-0.mt-3.text-center La medida se toma teniendo en cuenta la figura 3D del cuerpo humano, por ejemplo, todo el contorno de la cintura.
+
+
+    .tarjeta.color-primario.p-3(data-aos="fade").mb-4
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-1
+          img(src="@/assets/curso/t1/play.png")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              h4.mb-1 Clasificación de medidas
+              p.mb-0 Descubra los fundamentos para desarrollar patrones de prendas de vestir, partiendo del reconocimiento de las diferentes formas corporales, la comprensión de conceptos básicos de anatomía y la construcción del molde base como herramienta para interpretar la estructura del cuerpo.
+            .col-sm-auto
+              a.boton.color-acento-botones.texto-blanco(:href="obtenerLink('/downloads/1_Anexo.pdf')" target="_blank")
+                span Ir al sitio
+                  i.fas.fa-play-circle
+
+
 
 
 </template>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,265 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Tema 3
+      h1 Análisis de medidas y cuadro de tallas
+    p.mb-5 Una vez comprendido el proceso de análisis y toma de medidas corporales, en este espacio se explicará cómo se construye un cuadro de tallas y cuál es su aplicabilidad en el área del patronaje, facilitando la estandarización de patrones y el ajuste adecuado de las prendas a diferentes tipos de cuerpos.
+
+    .subt.mb-5(data-aos="fade-right")
+      .row.align-items-center
+        .col-md-auto.col-3
+          img(src='@/assets/curso/t1/ico.png', alt='')
+        .col-md-auto.col-9.ps-0
+          h3.mb-0.text-black Proporción versus canon
+
+    p.mb-4 El concepto de proporción versus canon se refiere a cuántas veces la cabeza cabe en la altura total del cuerpo; para comprender esta relación, es importante diferenciar los siguientes términos:
+
+    .row.justify-content-center.align-items-end.mb-5
+      .col-lg-6.col-md-8.col-sm-12.p-0
+        img(src='@/assets/curso/t3/img1.png', alt='')
+      .col-lg-6.col-md-12.col-sm-12.bgamarilloclaro2.p-0
+        ul.lista-ul--color.fa-al.px-5.mt-5.mb-3.py-3
+          li 
+            i.fas.fa-chevron-circle-right
+            span.mb-0 #[b Canon:] representa un modelo idealizado del cuerpo humano, cuyas características físicas son consideradas casi perfectas dentro de su género; se utiliza como referencia para representar la figura humana en proporciones armónicas.
+          li 
+            i.fas.fa-chevron-circle-right
+            span.mb-4 #[b Proporción:] es la relación de equilibrio y simetría entre las partes que conforman un todo; este concepto es esencial en disciplinas como el arte, la moda, la arquitectura y el diseño, ya que permite mantener coherencia visual y funcionalidad.
+
+    .subt2.mb-5(data-aos="fade-right")
+      .row.align-items-center
+        .col-md-auto.col-3
+          img(src='@/assets/curso/t1/ico.png', alt='')
+        .col-md-auto.col-9.ps-0
+          h3.mb-0.text-black Importancia de la proporción en el diseño
+
+    p Aplicar correctamente la proporción en el diseño de prendas es clave para lograr composiciones armónicas que guarden coherencia entre las partes del modelo y su relación con el cuerpo humano; esta correspondencia favorece el ajuste, la estética y el equilibrio visual en cada creación.
+    p.mb-4 A continuación, se presenta un ejemplo de la figura humana masculina, representada bajo los principios de canon y proporción:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-6.col-md-8.col-sm-12
+        .row.bgverdemate.p-3
+          .titulo-sexto.color-acento-contenido.mt-3
+            h5 Figura 14.
+            |  Canon femenino con 8 cabezas
+          .row.align-items-center.justify-content-center
+            .col-lg-8.col-md-10.col-sm-12
+              img.mb-3(src='@/assets/curso/t3/fig14.png', alt='La figura representa el canon de proporción masculina basado en ocho cabezas. El cuerpo está segmentado horizontalmente en ocho partes iguales, cada una equivalente al tamaño de la cabeza, lo que sirve como referencia para el estudio y diseño de proporciones corporales en anatomía artística o moda.')
+
+      .col-lg-6.col-md-8.col-sm-12
+        .row.bgrosado.mx-2.p-3
+          .titulo-sexto.color-acento-contenido.mt-3
+            h5 Figura 15.
+            |  Proporción de la figura humana Vitruvian
+          .row.align-items-center.justify-content-center
+            .col-lg-11.col-md-11.col-sm-12
+              img.mb-3(src='@/assets/curso/t3/fig15.png', alt='La imagen presenta el Hombre de Vitruvio, un dibujo anatómico clásico de Leonardo da Vinci. Representa una figura humana masculina desnuda con brazos y piernas extendidos en dos posiciones superpuestas. El cuerpo está inscrito dentro de un círculo y un cuadrado, simbolizando las proporciones ideales del ser humano. El trazo es lineal y detallado.')
+
+    .row.justify-content-center.align-items-end.mb-5(data-aos="fade")
+      .col-lg-4.col-md-6.col-sm-12 
+        img.p-0(src='@/assets/curso/t3/img2.png', alt='')
+      .col-lg-8.col-md-12.col-sm-12.p-0.mt-1
+        .row.bgamarilloclaro2.p-0
+          .col-auto.px-5.mt-5
+            p.bgamarillo.text-white.p-1.mb-4 #[b Importante]
+          p.px-5.mb-5.mt-3 #[em Vitruvian], proviene del latín #[em vitruvianus], en referencia al arquitecto romano #[b Marco Vitruvio]. El término se hizo popular por esta obra de Leonardo da Vinci, el #[b “Hombre de Vitruvio”], que representa las proporciones ideales del cuerpo humano según los principios de Vitruvio.
+
+    .subt.mb-5(data-aos="fade-right")
+      .row.align-items-center
+        .col-md-auto.col-3
+          img(src='@/assets/curso/t1/ico.png', alt='')
+        .col-md-auto.col-9.ps-0
+          h3.mb-0.text-black Proporciones corporales
+    
+    p.mb-4 La proporción corporal hace referencia a la relación matemática y armoniosa entre las partes del cuerpo humano (Esparza, 1993). Esta proporción está dada en todas las etapas del desarrollo del ser humano, desde su nacimiento hasta la vejez.
+
+    .row.justify-content-center.align-items-center
+      .col-lg-8.col-md-10.col-sm-12
+        .titulo-sexto.color-acento-contenido.mt-3
+          h5 Figura 16.
+          | Proporción de acuerdo con las etapas de crecimiento
+        figure
+          img.mb-3(src='@/assets/curso/t3/fig16.png', alt='La figura representa el canon de proporción masculina basado en ocho cabezas. El cuerpo está segmentado horizontalmente en ocho partes iguales, cada una equivalente al tamaño de la cabeza, lo que sirve como referencia para el estudio y diseño de proporciones corporales en anatomía artística o moda.')
+        figcaption Fuente: https://lapizuri.weebly.com/proporcioacuten-del-cuerpo-humano.html
+
+    Separador  
+    #t_3_1.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+      h2 3.1  Medidas deducidas
+
+    .row.justify-content-center.align-items-end.mb-5
+      .col-lg-3.col-md-12.col-sm-12
+        .row.bgamarilloclaro2.borde-redondo.px-4.py-4
+          p.mt-5 Las medidas deducidas hacen alusión a un proceso que se realiza una vez ya se han obtenido las medidas corporales del cliente, estas se utilizan para calcular un cuadro de tallas completo. Por lo general se hace uso de dos medidas; la altura o estatura total y el contorno superior prominente.
+          p.mt-4 A continuación, encontrará una tabla de fórmulas que le permitirá comprender cómo se realiza la deducción de medidas:
+      .col-lg-6.col-md-12.col-sm-12
+        .titulo-sexto.color-acento-contenido
+          h5 Tabla 3. 
+          |  Fórmulas de medidas deducidas
+        .tabla-a.color-secundario.estilo2.mt-4
+          table
+            thead
+              tr
+                th Parte del cuerpo
+                th Fórmula
+            tbody
+              tr
+                td Pecho
+                td Tórax máximo - 2
+              tr
+                td Cadera
+                td Tórax máximo + 4
+              tr
+                td Cintura
+                td Cadera - 14
+              tr
+                td Estatura 7ac (nota: 7ac hace referencia a la medida que se toma desde el cuello hasta la planta del pie)
+                td Estatura total - 20
+              tr
+                td Tallaje
+                td 3 / 10 de la estatura 7ac
+              tr
+                td Cintura/suelo
+                td 7 / 10 de la estatura 7ac
+              tr
+                td Largo del brazo
+                td 4 / 10 de la estatura 7ac
+              tr
+                td Estatura total
+                td Estatura 7ac + 20 cm
+              tr
+                td Base
+                td Pecho + desahogo (4 cm) + costura 2 cm
+
+      .col-lg-3.col-md-12.col-sm-12
+        .row.bgrosado.borde-redondo.px-4
+          p.mb-4.mt-4 A partir de la tabla anterior, es fundamental considerar tanto la medida base como la correcta aplicación de la fórmula. A continuación, se presenta un ejemplo:
+          ul.lista-ul--color.fa-al
+            li 
+              i.fas.fa-chevron-circle-right
+              span.mb-0 Para deducir la medida desde la 7.ª cervical hasta el punto final del cuerpo (denominada largo útil), se debe restar a la estatura total un valor aproximado de 20 centímetros, correspondiente a la longitud estandarizada de la cabeza.
+            li 
+              i.fas.fa-chevron-circle-right
+              span.mb-0 #[b Fórmula:] largo útil = estatura total - 20 cm.
+
+    p.mb-4 En la siguiente tabla se encuentran otras fórmulas utilizadas para deducir medidas horizontales y verticales a partir de valores base:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12.col-md-12.col-sm-12
+        .titulo-sexto.color-acento-contenido
+          h5 Tabla 4. 
+          |  Fórmulas para deducción de medidas corporales
+        .tabla-a.color-secundario.estilo3.mt-4
+          table 
+            caption Nota: 7ac hace referencia a toma de medida del cuello hasta la planta del pie.
+            thead
+              tr
+                th Horizontales
+                th Verticals
+                th Otras fórmulas de deducción
+            tbody
+              tr
+                td 
+                  ul.lista-ul--color.fa-al
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Tórax máximo = busto.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Tórax máximo = talla + 36 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Pecho = tórax máximo - 2 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Bajo busto = busto – 6 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Separación de busto = 2 / 5 busto. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cintura = busto – 10 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cadera = busto + 4 cm.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cuello =1 / 2 busto + 12 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Rodilla = 1 / 4 busto + 24 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Tobillo = 1 / 8 busto + 15,5 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Brazo = busto – 18 cm.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Muñeca = 1 / 4 busto + 5 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Ancho espalda = busto – 8 cm.
+                td 
+                  ul.lista-ul--color.fa-al
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Estatura 7ac = busto + 96 cm.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Estatura total = estatura 7ac + 20 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Altura de busto = 3 / 8 estatura 7ac – 27 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Altura bajo busto = 3 / 8 estatura 7ac – 18 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Largo brazo = 4 / 10 estatura 7ac.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cintura/suelo = 7 / 10 estatura 7ac.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Altura de cadera = 2 / 3 de tiro.
+                td 
+                  ul.lista-ul--color.fa-al
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Talla = años.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Pecho = años + 24 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cintura = 1 / 2 de años + 26 cm.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cadera = años + 28.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cuello = 2 / 3 de pecho + 4 cm. 
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Estatura 7ac = años x 5 + 65 cm.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Talle espalda = 3 / 10 de estatura 7ac.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Estatura total: estatura 7ac + 15 cm a 18 cm. Corresponden al canon del niño o medida de cabeza.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Largo de brazo= 4 / 10 de estatura 7ac.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Codo = 1 / 2 de brazo. Esta medida sin tener en cuenta lo que corresponde a la muñeca (menos 5 cm para todas las tallas).
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Cintura al suelo = 7 / 10 de estatura 7ac.
+                    li 
+                      i.fas.fa-chevron-circle-right
+                      span.mb-0 Largo de falda para hombre = 2 / 3 de la medida cintura al suelo – 3 cm.
+
 
 </template>
 
