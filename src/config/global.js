66acf95a22c19c5caac147aed5372e6669b69dde
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-07-11 14:52:35 -0500

    Cargue CFA, guion video intro

--- src/config/global.js
+++ src/config/global.js
@@ -185,7 +185,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_52450482_DU.pdf',
       },
       {
         icono: 'fas fa-download',
